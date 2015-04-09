8de57ecd2d44fff953653ff1df38f683f12cd52c
johanbijlsma <johanbijlsma24@gmail.com>
2015-04-09 09:39:21 +0200

    updates, and fixes
    added more content and made some bug fixes

---
+++ javascripts/function.js
@@ -0,0 +1,16 @@
+$(function() {
+
+	$( "#work div.link.IA" ).click(function() {
+		  $( "ul.detail.IA" ).slideToggle( "slow" );
+		});
+
+		$( "#work div.link.CA" ).click(function() {
+		  $( "ul.detail.CA" ).slideToggle( "slow" );
+		});
+
+
+
+
+
+	});
+
