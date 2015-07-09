f692692237266d2d2dd2ff2ddb2e602e39fe7828
johanbijlsma <johanbijlsma24@gmail.com>
2015-07-09 17:14:47 +0200

    updates to portfolio

--- javascripts/function.js
+++ javascripts/function.js
@@ -7,10 +7,23 @@ $(function() {
 		$( "#work div.link.CA" ).click(function() {
 		  $( "ul.detail.CA" ).slideToggle( "slow" );
 		});
-
-
-
-
-
 	});
+
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	    });
+	});
+});
+
+
+
 
