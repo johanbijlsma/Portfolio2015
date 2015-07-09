2d076a77bb387812cd71d48946c5286533af4392
johanbijlsma <johanbijlsma24@gmail.com>
2015-07-09 17:20:13 +0200

    small changes

--- javascripts/function.js
+++ javascripts/function.js
@@ -7,13 +7,23 @@ $(function() {
 		$( "#work div.link.CA" ).click(function() {
 		  $( "ul.detail.CA" ).slideToggle( "slow" );
 		});
-
-
-	$("li.work a").click(function(){
-		$("#work").slideDown(1000);
-	})
-
-
-
 	});
+
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	    });
+	});
+});
+
+
+
 
